5253eae3191de6b41ee9d3327fa0f24d159c0598
riha908 <petr.riha031008@gmail.com>
2026-01-22 20:20:44 +0100

    vytvoreni oblast a spojeni s main

---
+++ src/Oblast.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+public class Oblast {
+
+    private String id;
+    private String jmeno;
+    private Map<String, String> propojeni;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+
+    }
+

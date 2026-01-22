c1c0fb89c778490f1930fe62371c754887d5bf1a
riha908 <petr.riha031008@gmail.com>
2026-01-22 20:18:07 +0100

    pridani gson na praci s json

---
+++ src/HerniData.java
@@ -0,0 +1,47 @@
+import com.google.gson.Gson;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class HerniData {
+
+    public ArrayList<Item> items;
+    public ArrayList<HerniCHarakteri> characters;
+    public ArrayList<Oblast> locations;
+
+    public static HerniData loadGameDataFromResources(String resourcePath) {
+        //Vytvoření objektu pro práci s JSON souborem
+        Gson gson = new Gson();
+
+        //Načtení souboru
+        try (InputStream is = Main.class.getResourceAsStream(resourcePath)) {
+
+            // zdali soubor existuje
+            if (is == null) {
+                throw new IllegalStateException("Nenalezen resource: " + resourcePath +
+                        " (zkontrolujte, že soubor je v src/main/resources).");
+            }
+
+            //Přečte celý JSON a vytvoří instanci HerniData, naplní vlastnosti podle názvů klíčů v JSONU, vrátí se hotová třída HerniData
+            return gson.fromJson(
+                    new InputStreamReader(is, StandardCharsets.UTF_8),
+                    HerniData.class
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Chyba při načítání JSON: " + e.getMessage());
+        }
+    }
+
+
+    public Oblast findLocation(String id) {
+        for (Oblast o : locations) {
+            if (o.getId().equals(id)){
+                return o;
+            }
+        }
+        throw new IllegalArgumentException("Neexistuje mistnost s id: " + id);
+    }
+
+
+}

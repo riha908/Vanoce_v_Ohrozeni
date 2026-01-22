f4a5a1127739e5ca60492312ef18eddfd506da80
riha908 <petr.riha031008@gmail.com>
2026-01-22 20:20:18 +0100

    pridani zakladnich veci

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+
 public class Main {
     public static void main(String[] args) {
 
+        //Tohle bude fajn schovat do nějaké třídy, ve které budeme pracovat se všemi daty a hráčem
+        HerniData data = HerniData.loadGameDataFromResources("/mapa.json");
+
+        //Zkouška, zdali načtení proběhlo v pořádku
+        System.out.println("Items: " + data.items.size());
+        System.out.println("Characters: " + data.characters.size());
+        System.out.println("Locations: " + data.locations.size());
 
 
+        //Nalezení konkrétní lokace, se kterou je pak možno dále pracovat
+        System.out.println("Start lokace: " + data.findLocation("UbytovnaSkritku").getJmeno());
 
-    }
-}
+    }}
+

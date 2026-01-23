a0d353a0a722581e0bde348e741483c8707411cb
riha2 <email@gmail.com>
2026-01-23 09:52:38 +0100

    vytvoreni tridy Pomoc a predelani interface Command na abstract

--- src/prikazy/Command.java
+++ src/prikazy/Command.java
@@ -1,6 +1,13 @@
 package prikazy;
 
-public interface Command {
-    void execute(String parametr);
+public abstract class Command {
 
-}
+    protected String prikaz;
+
+    public void setPrikaz(String prikaz) {
+        this.prikaz = prikaz;
+    }
+
+    public abstract String execute();
+
+    public abstract boolean exit();}
---
+++ src/prikazy/Pomoc.java
@@ -0,0 +1,16 @@
+package prikazy;
+
+public class Pomoc extends Command{
+
+
+        @Override
+        public String execute() {
+            return "";
+        }
+
+        @Override
+        public boolean exit() {
+            return false;
+        }
+    }
+

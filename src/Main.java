4a0baccb1441bf006d8a0491eaffa210527e9f12
riha2 <email@gmail.com>
2026-01-23 10:28:21 +0100

    pridani do Mainu start konzole

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
-
 public class Main {
     public static void main(String[] args) {
 
-        //Tohle bude fajn schovat do nějaké třídy, ve které budeme pracovat se všemi daty a hráčem
-        HerniData data = HerniData.loadGameDataFromResources("/mapa.json");
-
-        //Zkouška, zdali načtení proběhlo v pořádku
-        System.out.println("Items: " + data.items.size());
-        System.out.println("Characters: " + data.characters.size());
-        System.out.println("Locations: " + data.locations.size());
-
-
-        //Nalezení konkrétní lokace, se kterou je pak možno dále pracovat
-        System.out.println("Start lokace: " + data.findLocation("UbytovnaSkritku").getJmeno());
-
+        Konzole k = new Konzole();
+        k.start();
     }}
 

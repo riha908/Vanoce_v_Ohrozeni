ced4eee6873990d5a12e5235e0f45c2184757937
riha2 <riha2@spsejecna.cz>
2026-01-26 12:53:57 +0100

    pridane vypsani itemu v inventari

---
+++ src/prikazy/Inventar.java
@@ -0,0 +1,25 @@
+package prikazy;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Inventar extends Command{
+    @Override
+    public String execute() {
+        ArrayList<String> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Konzole.batoh))) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+            return list.toString();
+        }catch(Exception e){
+            return "Inventar nefunguje";
+        }
+    }
+    @Override
+    public boolean exit() {
+        return false;
+    }
+}

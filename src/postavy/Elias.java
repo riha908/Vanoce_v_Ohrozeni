208ad71bc4fdfe6f9854ca141426f5c917028db3
riha2 <email@gmail.com>
2026-02-06 10:25:18 +0100

    pridani metod do Elias

--- src/postavy/Elias.java
+++ src/postavy/Elias.java
@@ -1,6 +1,34 @@
 package postavy;
 
-public class Elias extends Charakteri{
+import Hra.Item;
+import Hra.Oblast;
 
-   public void pouzij(){}
+import java.util.ArrayList;
+
+public class Elias extends Charakteri{
+   private Oblast location;
+   private ArrayList<Item> inventory;
+
+   public Elias(){
+      this.inventory = new ArrayList<>();
+   }
+
+   public Oblast getLocation() {
+      return location;
+   }
+
+   public void setLocation(Oblast location) {
+      this.location = location;
+   }
+
+   public ArrayList<Item> getInventory() {
+      return inventory;
+   }
+
+   public void addToInventory(Item item) {
+      if (item == null) {
+         throw new IllegalArgumentException("Item nesmí být null");
+      }
+      inventory.add(item);
+   }
 }

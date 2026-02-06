62486bc4c451ccb857fafe1b223324a2e491d2f5
riha2 <email@gmail.com>
2026-02-06 09:40:55 +0100

    odebrani zbytecnych veci

--- src/Hra/Main.java
+++ src/Hra/Main.java
@@ -2,16 +2,6 @@ package Hra;
 
 public class Main {
     public static void main(String[] args) {
-//Tohle bude fajn schovat do nějaké třídy, ve které budeme pracovat se všemi daty a hráčem
-        HerniData data = HerniData.loadGameDataFromResources("/mapa.json");
-
-        //Zkouška, zdali načtení proběhlo v pořádku
-       // System.out.println("Items: " + data.items.size());
-       // System.out.println("Characters: " + data.characters.size());
-      //  System.out.println("Locations: " + data.lokace.size());
-
-        // Nalezení konkrétní lokace, se kterou je pak možno dále pracovat
-        System.out.println("Start lokace: " + data.findLocation("UbytovnaSkritku").getJmeno());
 
         Konzole k = new Konzole();
         k.start();
